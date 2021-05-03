4da4c0743f60594250601c1012768d5d118dbe56
GulshanSS <sahugulshan686@gmail.com>
2021-05-03 13:35:16 +0530

    DTO for Insurance Products

---
+++ PESMVC.Data.Models.DTO/InsuranceProducts/CreateInsuranceProductRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESMVC.Data.Models.DTO.InsuranceProducts
+{
+    public class CreateInsuranceProductRequest
+    {
+        [Required]
+        public string productName { get; set; }
+        [Required]
+        public string productLine { get; set; }
+    }
+}
