2fcf6534cf45160d68b34de46d8bc2fe16a7cf61
Pratik-sys <pithore.pratik@gmail.com>
2021-05-04 09:29:51 +0530

    Update DTO for Policy

---
+++ PESMVC.Data/DTO/Policy/SearchPolicyByCustomerIdAndPolicyNumberRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESMVC.Data.Models.DTO.Policy
+{
+    public class SearchPolicyByCustomerIdAndPolicyNumberRequest
+    {
+        [Required]
+        public string customerId { get; set; }
+        [Required]
+        public string policyNumber { get; set; }
+    }
+}
---
+++ PESMVC.Data/DTO/Policy/UpdatePolicyRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESMVC.Data.Models.DTO.Policy
+{
+    public class UpdatePolicyRequest
+    {
+        [Required]
+        public string customerId { get; set; }
+        [Required]
+        public string policyNumber { get; set; }
+        [Required]
+        public string policyNominee { get; set; }
+        [Required]
+        public string policyNomineeRelation { get; set; }
+        [Required]
+        public string policyPremiumPaymentFrequency { get; set; }
+        [Required]
+        public string filePath { get; set; }
+        [Required]
+        public string customerAddress { get; set; }
+        [Required]
+        public string customerTelephone { get; set; }
+        [Required]
+        public bool customerSmoker { get; set; }
+    }
+}
