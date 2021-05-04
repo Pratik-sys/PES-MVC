004fafbcc3cf020267b42bccc1102d57ad6d39b4
Pratik-sys <pithore.pratik@gmail.com>
2021-05-04 09:29:23 +0530

    Update DTO for Policy

---
+++ PESMVC.Data/DTO/Customer/CreateCustomerRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESMVC.Data.Models.DTO.Customer
+{
+    public class CreateCustomerRequest
+    {
+        [Required]
+        public string customerName { get; set; }
+        [Required]
+        public string customerAddress { get; set; }
+        [Required]
+        public string customerTelephone { get; set; }
+        [Required]
+        public string customerGender { get; set; }
+        [Required]
+        public System.DateTime customerDOB { get; set; }
+        [Required]
+        public bool customerSmoker { get; set; }
+        [Required]
+        public string customerHobbies { get; set; }
+    }
+}
---
+++ PESMVC.Data/DTO/Customer/SearchCustomerByIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PESMVC.Data.Models.DTO.Customer
+{
+    public class SearchCustomerByIdRequest
+    {
+        [Required]
+        public string customerId { get; set; }
+    }
+}
