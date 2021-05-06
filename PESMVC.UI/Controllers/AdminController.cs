049f2013f1f3e2956bbfde8cd5f5c1f865c63391
Pratik-sys <pithore.pratik@gmail.com>
2021-05-06 21:41:52 +0530

    Add Admin Controller and view

---
+++ PESMVC.UI/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PESMVC.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EndorsmentApproval()
+        {
+            return View();
+        }
+        public ActionResult InsuranceProduct()
+        {
+            return View();
+        }
+        public ActionResult SearchCustomer()
+        {
+            return View();
+        }
+
+        public ActionResult SearchPolicy()
+        {
+            return View();
+        }
+        public ActionResult EndorsementDetails()
+        {
+            return View();
+        }
+    }
+}
